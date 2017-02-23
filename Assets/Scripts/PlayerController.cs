15cb1b36c8262228c8237d4ddf9609915062c053
Roger Rodríguez Camprubí <roger.rodriguez@king.com>
2017-02-23 19:21:19 +0100

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Vector2 velocityIndicator;
+
+	private Rigidbody2D rb2d;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate()
+	{
+		// update velocity
+		velocityIndicator = rb2d.velocity;
+
+		// apply input force
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 inputMovement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (inputMovement*5);
+	}
+}
