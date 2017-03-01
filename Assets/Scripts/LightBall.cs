9029fffcdfd220752692ad1b4a9eeb606a37e677
necrobooder <roger.booder@gmail.com>
2017-03-01 01:54:26 +0100

    First iteration at lightballs

---
+++ Assets/Scripts/LightBall.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBall : MonoBehaviour {
+
+	public Direction direction;
+	public int speed;
+
+	public Vector2 velocity;
+	private Rigidbody2D rb2d;
+
+	void Start () {
+		// Get the rigid body component
+		rb2d = GetComponent<Rigidbody2D> ();
+		if (!rb2d)
+			print ("no rigidbody found");
+	}
+
+	public void SetUp(Direction direction, int speed)
+	{
+		this.direction = direction;
+		this.speed = speed;
+
+		switch (direction) {
+		case Direction.north:
+			velocity = new Vector2 (0, speed);
+			break;
+		case Direction.south:
+			velocity = new Vector2 (0, -speed);
+			break;
+		case Direction.east:
+			velocity = new Vector2 (speed, 0);
+			break;
+		case Direction.west:
+			velocity = new Vector2 (-speed, 0);
+			break;
+		default:
+			break;
+		}
+
+		if (!rb2d)
+			print ("none found");
+		else
+			rb2d.velocity = velocity;
+	}
+
+	void FixedUpdate()
+	{
+		if (!rb2d)
+			print ("none found");
+		else
+			rb2d.velocity = velocity;
+	}
+}
---
+++ Assets/Scripts/LightBallSpawner.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBallSpawner : MonoBehaviour {
+
+	private const float RANGE = 16f;
+
+	public float spawnTimer;
+	public LightBall objectToSpawn;
+
+	private float counter;
+
+	void Start()
+	{
+		counter = spawnTimer;
+	}
+
+	void Update () {
+		ManageSpawnTimes ();
+	}
+
+	void ManageSpawnTimes()
+	{
+		counter -= Time.deltaTime;
+		if (counter < 0) 
+		{
+			counter = spawnTimer;
+			SpawnObject ();
+		}
+	}
+
+	void SpawnObject()
+	{
+		// get parameters
+		Direction direction = GetRandomDirection();
+		int speed = (int)(Random.value * 5f);
+		Vector2 spawnPosition = new Vector2 (Random.Range (-RANGE, RANGE), Random.Range (-RANGE, RANGE));
+
+		// Instantiate
+		LightBall lightball = Instantiate (objectToSpawn, spawnPosition, Quaternion.identity) as LightBall;
+
+		// Set params
+		lightball.SetUp(direction, speed);
+	}
+
+	Direction GetRandomDirection()
+	{
+		float x = Random.value;
+		if (x < .25f)
+			return Direction.north;
+		if (x < .5f)
+			return Direction.south;
+		if (x < .75f)
+			return Direction.east;
+		return Direction.west;
+	}
+}
