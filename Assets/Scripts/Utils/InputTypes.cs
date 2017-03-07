6eff049e387f9e353bebf9fa8f4ad96ba0591d99
necrobooder <roger.booder@gmail.com>
2017-03-07 01:42:34 +0100

    Add InputTypes static class

---
+++ Assets/Scripts/Utils/InputTypes.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputTypes{
+
+	public static string HORIZONTAL_MOVEMENT = "H_Movement";
+	public static string VERTICAL_MOVEMENT = "V_Movement";
+	public static string HORIZONTAL_ROTATION = "H_Rotation";
+	public static string VERTICAL_ROTATION = "V_Rotation";
+	public static string STRONG_HIT = "STRONG_Hit";
+	public static string WEAK_HIT = "WEAK_Hit";
+}
